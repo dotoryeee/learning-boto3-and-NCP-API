36b144ee5d32fa8ea0546625e01652d384c11632
dotoryeee <bayer@kakao.com>
2021-05-25 10:51:13 +0900

    fix: str to list

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 import boto3
 from botocore.exceptions import ClientError
 
-TARGET_INSTANCE_IDs = []
+TARGET_INSTANCE_IDs = ["i-082ec8f7f04524c30"]
 
 ec2 = boto3.client("ec2")
 
 
-def stopInstance():
-    for instanceID in TARGET_INSTANCE_IDs:
-        try:
-            response = ec2.stop_instances(InstanceIDs=instanceID, DryRun=False)
-            print(f"STOP SUCCESS : {response}")
-        except ClientError as e:
-            print(f"ERROR : {e}")
+def stopInstance(instanceID):
+    try:
+        response = ec2.stop_instances(InstanceIds=instanceID, DryRun=False)
+        print(f"STOP SUCCESS : {response}")
+    except ClientError as e:
+        print(f"ERROR : {e}")
 
 
-def startInstance():
-    for instanceID in TARGET_INSTANCE_IDs:
-        try:
-            response = ec2.start_instances(InstanceIDs=instanceID, DryRun=False)
-            print(f"START SUCCESS : {response}")
-        except ClientError as e:
-            print(f"STOP ERROR : {e}")
+def startInstance(instanceID):
+    try:
+        response = ec2.start_instances(InstanceIds=instanceID, DryRun=False)
+        print(f"START SUCCESS : {response}")
+    except ClientError as e:
+        print(f"STOP ERROR : {e}")
 
 
-def rebootInstance():
-    for instanceID in TARGET_INSTANCE_IDs:
-        try:
-            response = ec2.reboot_instances(InstanceIDs=instanceID, DryRun=False)
-            print(f"REBOOT SUCCESS : {response}")
-        except ClientError as e:
-            print(f"REBOOT ERROR : {e}")
+def rebootInstance(instanceID):
+    try:
+        response = ec2.reboot_instances(InstanceIds=instanceID, DryRun=False)
+        print(f"REBOOT SUCCESS : {response}")
+    except ClientError as e:
+        print(f"REBOOT ERROR : {e}")
+
+
+def main():
+    print("---TARGET INSTANCES---")
+    for instance in TARGET_INSTANCE_IDs:
+        print(instance, end=", ")
+    stopInstance(TARGET_INSTANCE_IDs)
+
+
+main()
