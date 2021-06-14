dd7f54e3d4815ad9c265170c3f9f2247556ed3de
dotoryeee <bayer@kakao.com>
2021-06-14 09:48:11 +0900

    add: EC2 Snapshot

---
+++ AWS/EC2SnapShot.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import boto3
+
+def lambda_handler(event, context):
+    ec2 = boto3.resource('ec2', region_name="ap-northeast-2")
+
+    instances = ec2.instances.filter(
+        Filters=[
+            {'Name': 'tag:backup', 'Values': ['true']}
+        ]
+    )
+
+    timestamp = datetime.utcnow().isoformat()
+
+    for instance in instances.all():
+        print('='*18, end='')
+        print(f'INSTANCE {instance.id} SNAPSHOT', end='')
+        print('='*18)
+        for volume in instance.volumes.all():
+            print(f'VOLUME : {volume.id} ..... ', end='')
+            snapshot = volume.create_snapshot(Description=f'{instance.id}/{volume.id}/{timestamp}')
+            print(f"SNAPSHOT ID : {snapshot.id}")
+
+lambda_handler('','')
